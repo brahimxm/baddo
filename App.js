f1ddbb13ee94b8fcc712d419c37b3c4dfe2aadee
alphabboss <ibgazachamp.30@gmail.com>
2024-11-22 16:46:51 +0100

    my app is up running

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>My app is up running</Text>
       <StatusBar style="auto" />
     </View>
   );
